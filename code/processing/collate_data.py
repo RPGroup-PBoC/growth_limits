d23da48607f33a670ac8ca5b0284d60689c0a408
Griffin Chure <gchure@caltech.edu>
2020-02-02 13:32:31 -0800

    Update of datasets. Added Ishihama 2008

--- code/processing/collate_data.py
+++ code/processing/collate_data.py
@@ -7,3 +7,5 @@ files = glob.glob('../../../data/*longform_annotated.csv')
 dfs = [pd.read_csv(f) for f in files]
 df = pd.concat(dfs, sort=False)
 df.to_csv('../../../data/compiled_datasets.csv', index=False)
+
+# %%

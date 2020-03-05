9b69e4ac777629baefa0822af4019330a0ae153c
Griffin Chure <gchure@caltech.edu>
2020-03-05 15:26:42 -0800

    Finished incorporating the valgepea data'

--- code/processing/valgepea_2013_munging/generate_tidy_data.py
+++ code/processing/valgepea_2013_munging/generate_tidy_data.py
@@ -7,43 +7,51 @@ import tqdm
 counts = pd.read_csv('../../../data/valgepea2013_raw_data/valgepea2013_copynums.csv')
 
 # Load the annotation list.
-colicogs = pd.read_csv('../../../data/escherichia_coli_gene_annotations.csv')
+colicogs = pd.read_csv('../../../data/ecoli_genelist_master.csv')
 
 # Instantiate a blank dataframe to which measurements and annotations will be
 # added
-df = pd.DataFrame([])
+dfs = []
 
 # Iterate through each gene in the count data.
 for g, d in tqdm.tqdm(counts.groupby(['gene', 'growth_rate_hr-1']), desc="Iterating through genes..."):
-
     # Determine number of entries per gene
-    gene = colicogs[colicogs['gene_name'].str.lower()==g[0].split(',')[0]]
-    if len(g) > 0:
-        for i in range(len(gene)):
-            _gene = gene.iloc[i]
-            cog_class = _gene['cog_class']
-            cog_cat = _gene['cog_category']
-            cog_letter = _gene['cog_letter']
-            mass = _gene['mass_da']
-            annotation = _gene['annotation']
-            # extract relevant information.
-            gene_dict = {
+    gene = colicogs[colicogs['gene_name'].str.lower()==g[0].split(',')[0].lower()]
+      
+    if len(gene) > 0:
+        cog_class = _gene['cog_class']
+        cog_cat = _gene['cog_category']
+        cog_letter = _gene['cog_letter']
+        gene_product = _gene['gene_product']
+        go_term = ';'.join(list(gene['go_terms'].unique()))
+        mw = gene['mw_kda'].values[0]
+        if str(mw) == 'nan':
+            mw = str(0)
+        elif '//' in str(mw):
+            mw = mw.split('//')[0]
+        mw = float(mw)
+        # extract relevant information.
+        gene_dict = {
                     'gene_name': gene['gene_name'].unique()[0],
+                    'b_number':b_number,
                     'condition': 'glucose_minimal',
                     'tot_per_cell': d['copy_number'].values[0],
+                    'fg_per_cell':d['copy_number'].values[0] * mw * 1E3 * 6.022E-8, 
                     'cog_class': cog_class,
                     'cog_category': cog_cat,
                     'cog_letter': cog_letter,
-                    'mass_da': mass,
-                    'annotation': annotation,
-                    'growth_rate_hr': g[1]
-                    }
-            df = df.append(gene_dict, ignore_index=True)
+                    'annotation': gene_product,
+                    'growth_rate_hr': g[1],
+                    'go_terms':go_term,
+        }
+        dfs.append(pd.DataFrame(gene_dict, index=[0]))
+    else:
+        print(g[0].split(',')[0])
 
 #%%
 # Compute the mass per cell and include dataset notation.
-df['fg_per_cell'] = df['mass_da'].values * df['tot_per_cell'].values * 6.022E-8
-df['dataset'] = 'valgepea2013'
+df['dataset'] = 'valgepea_2013'
+df['dataset_name'] = 'Valgepea et al. 2013'
 df['strain'] = 'MG1655'
 df.to_csv('../../../data/valgepea2013_longform_annotated.csv')
 # %%

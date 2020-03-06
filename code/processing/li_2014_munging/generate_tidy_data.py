1d3c394247f6e9b6fabbf27c1dce000c2200eb3a
Griffin Chure <gchure@caltech.edu>
2020-03-05 18:57:21 -0800

    Update of compiled datasets

--- code/processing/li_2014_munging/generate_tidy_data.py
+++ code/processing/li_2014_munging/generate_tidy_data.py
@@ -2,8 +2,6 @@
 import numpy as np
 import pandas as pd
 import tqdm
-# Define teh constant of total number of genes identified. 
-N_GENES = 3041
 
 # From the SI of Li 2014, Cell, hard-code the growth rates. 
 growth_rates = {'MOPS complete': np.log(2) / (21.5 / 60), 
@@ -19,43 +17,55 @@ synthesis_tidy = synthesis_rates.melt('Gene')
 synthesis_tidy = synthesis_tidy[~synthesis_tidy['value'].str.contains('\[')]
 
 # Load the annotation list. 
-colicogs = pd.read_csv('../../../data/escherichia_coli_gene_annotations.csv')
+colicogs = pd.read_csv('../../../data/ecoli_genelist_master.csv')
 
 # Instantiate a blank dataframe to which measurements and annotations will be
 # added
-df = pd.DataFrame([])
+dfs = [] 
 for g, d in tqdm.tqdm(synthesis_tidy.groupby('Gene'), desc='Iterating through genes'):
-    gene = colicogs.loc[colicogs['gene_name']==g]
-
-    if len(g) > 0:
-        for i in range(len(gene)):
-            _gene = gene.iloc[i]
-            cog_class = _gene['cog_class']
-            cog_cat = _gene['cog_category']
-            cog_letter = _gene['cog_letter']
-            mass = _gene['mass_da']
-            annotation = _gene['annotation']
-
+    if '+' in g:
+        _gene = g.split('+')
+        split_factor = 2
+    else:
+        _gene = [g]
+        split_factor=1
+    for _g in _gene:
+        gene = colicogs.loc[colicogs['gene_name'].str.lower()==_g.lower()]
+        if len(gene) > 0:
+            cog_class = gene['cog_class'].values[0]
+            cog_cat = gene['cog_category'].values[0]
+            cog_letter = gene['cog_letter'].values[0]
+            b_number = gene['b_number'].unique()[0]
+            gene_product = gene['gene_product'].unique()[0]
+            go_term = ';'.join(list(gene['go_terms'].unique()))
+            mw = gene['mw_kda'].values[0]
+            if str(mw) == 'nan':
+                mw = 0
+            elif '//' in str(mw):
+                mw = str(mw).split('//')[0]
+            gene_product = gene['gene_product']
             # Iterate through each condition and extract relevant information. 
             for c, r in growth_rates.items():
                 if c in d['variable'].unique():
                     gene_dict = {
-                        'gene_name': g,
+                        'gene_name': _g,
                         'condition': c,
-                        'tot_per_cell': d[d['variable']==c]['value'].values[0],
+                        'tot_per_cell': split_factor**-1 * float(d[d['variable']==c]['value'].values[0]),
+                        'fg_per_cell': split_factor**-1 * float(d[d['variable']==c]['value'].values[0]) * float(mw) * 1E3 * 6.022E-8,
                         'cog_class': cog_class,
                         'cog_category': cog_cat,
                         'cog_letter': cog_letter,
-                        'mass_da': mass,
-                        'annotation': annotation,
-                        'growth_rate_hr': growth_rates[c]
-                        }
-                    df = df.append(gene_dict, ignore_index=True)
-
+                        'b_number':b_number,    
+                        'gene_product': gene_product,
+                        'growth_rate_hr': growth_rates[c]}
+                    dfs.append(pd.DataFrame(gene_dict, index=[0]))
+        else:
+            print(f'Warning!! {g} not found in list.')
+df = pd.concat(dfs, sort=False)
 #%%
 # Compute the mass per cell and include dataset notation. 
-df['fg_per_cell'] = df['mass_da'].values * df['tot_per_cell'].values * 6.022E-8
 df['dataset'] = 'li_2014'
+df['dataset_name'] = 'Li et al. 2014'
 df['strain'] = 'MG1655'
 df.to_csv('../../../data/li2014_longform_annotated.csv')
 # %%

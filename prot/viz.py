d2cd4e31bcea0608604334f2d9bc9d296a056152
Griffin Chure <gchure@caltech.edu>
2020-03-20 12:15:54 -0700

    Added summary plot of intersection

--- prot/viz.py
+++ prot/viz.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import squarify as sq
 from bokeh.themes import Theme
+import matplotlib.pyplot as plt
+import seaborn as sns
 import altair as alt
 import bokeh.io
 
@@ -146,3 +148,35 @@ def bokeh_theme():
               'light_purple': '#D4C2D9', 'dark_green':'#7E9D90', 'dark_brown':'#905426'}
     palette = [v for k, v in colors.items() if 'pale' not in k]
     return [colors, palette]
+
+
+def plotting_style(grid=False):
+    """
+    Sets the style to the publication style
+    """
+    rc = {'axes.facecolor': '#E3DCD0',
+          'font.family': 'Lucida Sans Unicode',
+          'grid.linestyle': '-',
+          'grid.linewidth': 0.5,
+          'grid.alpha': 0.75,
+          'grid.color': '#ffffff',
+          'axes.grid': grid,
+          'ytick.direction': 'in',
+          'xtick.direction': 'in',
+          'xtick.gridOn': grid,
+          'ytick.gridOn': grid,
+          'ytick.major.width':5,
+          'xtick.major.width':5,
+          'ytick.major.size': 5,
+          'xtick.major.size': 5,
+          'mathtext.fontset': 'stixsans',
+          'mathtext.sf': 'sans',
+          'legend.frameon': True,
+          'legend.facecolor': '#FFEDCE',
+          'figure.dpi': 150,
+           'xtick.color': 'k',
+           'ytick.color': 'k'}
+    plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
+    plt.rc('mathtext', fontset='stixsans', sf='sans')
+    sns.set_style('darkgrid', rc=rc)
+

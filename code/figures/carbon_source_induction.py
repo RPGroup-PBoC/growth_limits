546ae690f9a8ba62ca3c27e07792cc49444a40a4
Griffin Chure <gchure@caltech.edu>
2020-05-15 12:44:08 -0700

    Added panel labels to induced expression plot

--- code/figures/carbon_source_induction.py
+++ code/figures/carbon_source_induction.py
@@ -46,13 +46,20 @@ for g, d in data.groupby(['dataset', 'growth_rate_hr', 'condition']):
     for c, a in axes.items():
         _d = d[d['shorthand']==c]
         if (c.split('_tport')[0] in g[-1]) & ('glucose' not in g[-1]):
-            _color = 'red'
+            _color = colors['red']
             alpha=1
-            marker=dataset_markers[g[0]] 
+
+            if 'pAA' in g[-1]:
+                label = 'glycerol + A.A.'
+            else:
+                label = c.split('_tport')[0]
+            a.text(_d['growth_rate_hr'] + 0.05, _d['n_complex'],
+            label, fontsize=6)
         else:
             alpha = 0.5
             _color = colors['dark_green'] 
-            marker = dataset_markers[g[0]]
+
+        marker = dataset_markers[g[0]]
         a.plot(_d['growth_rate_hr'], _d['n_complex'], linestyle='none',
               marker=marker, ms=4, alpha=alpha,
         color=_color, markeredgewidth=0.5, markeredgecolor='k')
@@ -62,9 +69,13 @@ for g, d in data.groupby(['dataset', 'dataset_name']):
     _ax[-1].plot([], [], linestyle='none', marker=dataset_markers[g[0]], color=colors['dark_green'],
                 alpha=0.5, markeredgecolor='k', markeredgewidth=0.5, label=g[1],
                 ms=4)
-_ax[-1].plot([], [], 'rX', markeredgecolor='k', markeredgewidth=0.5, 
+_ax[-1].plot([], [], 's', color=colors['red'], markeredgecolor='k', markeredgewidth=0.5, 
             label='induced expression', ms=4)
 _ax[-1].legend(fontsize=7.5, loc='center')
 plt.tight_layout()
+
+# Add panel labels
+for a, lab in zip(ax.ravel(), ['(A)', '(B)', '(C)', '(D)', '(E)']):
+    a.text(-0.22, 1.1, lab, transform=a.transAxes, fontsize=6)
 plt.savefig('../../figures/induced_expression.pdf', bbox_inches='tight')
 # %%

c90342823529b6ba7b4d1e93718db44bba36466f
Griffin Chure <gchure@caltech.edu>
2020-07-01 08:52:52 -0700

    Added my comments throughtout the translation section

--- code/figures/fig9c_rRNA_scaling_nb.py
+++ code/figures/fig9c_rRNA_scaling_nb.py
@@ -21,25 +21,29 @@ n_ori = constants['N_ori']['value']
 k_rrna = 1 # functional rRNA units per second at steady state
 n_operon = 7 # average number of functional ribosomal operons per chromosome
 n_rRNA_full = n_operon * k_rrna * n_ori * t_double
-n_rRNA_half = 0.5*n_operon * k_rrna * n_ori * t_double
-n_rRNA_single = k_rrna * n_ori * t_double
-n_rRNA_limit = n_operon * k_rrna  * t_double
+n_rRNA_noparallel = n_operon * k_rrna * t_double
 
+
+# Instantiate the figure canvas
 fig, ax = plt.subplots(1,  1, figsize=(5, 4))
 ax.set_yscale('log')
 ax.set_xlabel('growth rate [hr$^{-1}$]')
 ax.set_ylabel(r'number of ribosomal units')
 ax.set_ylim([5E3, 8E5])
-# ax2 = ax.twinx()
-ax.plot(growth_rate, n_rRNA_full, '-', lw=1, color=colors['blue'],
-        label=r'rRNA production limit (parallelized, 7 operon copies)')
 
+# Plot the predicted maximal number of rRNA under different regimes
+ax.plot(growth_rate, n_rRNA_full, '-', lw=1, color=colors['blue'],
+        label='maximum number of ribosomes\nwith parallelization (rRNA units)')
+ax.plot(growth_rate, n_rRNA_noparallel, '--', lw=1, color=colors['blue'],
+        label='maximum number of ribosomes\nwithout parallelization (rRNA units)')
+
+# Plot the experimentally observed number of ribosomes
 for g, d in data.groupby(['dataset', 'dataset_name']):
     ax.plot(d['growth_rate_hr'], d['n_complex'], 'o',  ms=4, markeredgecolor='k',
-            markeredgewidth=0.5, color=dataset_colors[g[0]], label=g[1])
-
-
-# ax.legend(fontsize=6, bbox_to_anchor=(1, 1))
-plt.savefig('../../figures/fig9c_rRNA_scaling.pdf')
+            markeredgewidth=0.5, color=dataset_colors[g[0]], label='__nolegend__')
+            
+ax.legend(fontsize=8)
+# Save
+plt.savefig('../../figures/fig9c_rRNA_scaling.svg')
 
 # %%

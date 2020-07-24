6f425769085d319e363268cba9bd59b3a6da9ca6
Griffin Chure <gchure@caltech.edu>
2020-07-23 22:13:22 -0700

    Finished interactive figure for the model explorer

--- code/figures/js/model_explorer.js
+++ code/figures/js/model_explorer.js
@@ -1,17 +1,12 @@
 // Define variables
-var kd = kd_slider.value;
+var kd = kd_slider.value * 0.001;
 var r_aa = Math.pow(10, raa_slider.value);
 var fa = fa_slider.value;
 
-
 // Define constants. 
-var rt_max = 17.1; // in AA  / sec 
-var avo = 6.022E5; // Avogadro's number for conversion given milimolar concs at fL vol
-var rho = 1.1 // cell density in pg / fl
-var theta_prot =  0.15; // Fraction of dry mass that is protein
-var m_aa = 110 / 6.022E11; // Mass of average amino acid in pg
-var vol = 1 // in µm^3
-var Lr = 7500 // AA per ribosome
+const rt_max = 17.1; // in AA  / sec 
+const avo = 6.022E23; // Avogadro's number
+const V = 1E-15 ; // VOlume in liters
 
 // Define data sources
 var data = source.data;
@@ -31,12 +26,14 @@ for (var i = 0; i < R.length; i++) {
 }
 
 
-console.log(Ra)
+var NAA = compute_NAA(R);
+var vol = compute_vol(R);
+
 // Given updated values, compute new values. 
-var r_t = compute_elong_rate(R, r_aa, kd, vol, 1); 
-var r_t_fa = compute_elong_rate(R, r_aa, kd, vol, fa);
-var lambda = compute_growth_rate(r_t, R, vol, 1);
-var lambda_fa = compute_growth_rate(r_t, R, vol, fa);
+var r_t = compute_elong_rate(r_aa, R, kd, 1, V); 
+var r_t_fa = compute_elong_rate(r_aa, R, kd, fa, V);
+var lambda = compute_growth_rate(NAA, R, 1, vol);
+var lambda_fa = compute_growth_rate(NAA, R, fa, vol);
 data['R_fa'] = Ra;
 data['lambda'] = lambda;
 data['lambda_fa'] = lambda_fa;
@@ -45,13 +42,16 @@ data['elongation_rate_fa'] = r_t_fa;
 data['leg'] = leg;
 
 // Compute the consumption and production levels. 
+var R_cell = [];
 for (var i = 0; i <  R.length; i++) {
     supply[i] = r_aa;
     consumed[i] = r_t[i] * R[i];
     consumed_fa[i] = r_t[i] * R[i] * fa;
     accum[i] = r_aa - r_t[i] * R[i];
     accum_fa[i] = r_aa - r_t[i] * R[i] * fa;
+    R_cell[i] = vol[i] * R[i];
 }
+data['R_cell'] = R_cell;
 data['supply'] = supply;
 data['consumed'] = consumed;
 data['consumed_fa'] = consumed_fa;
@@ -65,31 +65,51 @@ source.change.emit();
 // Function Definition
 // -----------------------------------------------------------------------------
 
-function compute_NAA(vol) { 
-    // Computes the number of amino acids per cell based on a volume
-    return (rho * vol * theta_prot) / (m_aa);
-}
-
-function compute_elong_rate(R, r_aa, kd, vol, pact) { 
-    // Computes the quadratic solution for the elongation rate. 
-
-    // Compute the various components. 
-    var NAA = compute_NAA(vol);
+function compute_elong_rate(r_aa, R, Kd, fa) {
     var r_t = [];
     for (var i = 0; i < R.length; i++) {
-        var theta = (kd * vol * avo) / (Math.log(2) * (NAA + Lr * R[i]));
-        var root = Math.sqrt(4 * theta * rt_max * r_aa * R[i] * pact + Math.pow(rt_max * R[i] * pact, 2) - 2 * rt_max * r_aa * R[i] * pact + Math.pow(r_aa, 2));
-        r_t[i] = (rt_max * R[i] * pact + r_aa - root) / (2 * R[i] * pact * (1 - theta)); 
+        var a = -R[i] * fa;
+        var b = r_aa + rt_max * R[i] * fa + Kd * V * avo;
+        var c = -rt_max * r_aa;
+        var numer = -b + Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+        r_t[i] = numer / (2 * a);
     }
     return r_t
 }
 
-function compute_growth_rate(r_t, R, vol, pact) {
-    // Given  an elongation rate and a unit volume, compute the growth rate
-    var NAA = compute_NAA(vol);
-    var lambda = [];
+function compute_elong_rate_volume(r_aa, R, Kd, fa, volume) {
+    var r_t = [];
     for (var i = 0; i < R.length; i++) {
-        lambda[i] = 3600 * (r_t[i] * R[i] * pact)  / (NAA + R[i] * Lr); 
+        var a = -R[i] * fa;
+        var b = r_aa + rt_max * R[i] * fa + Kd * volume[i] * 1E-15 * avo;
+        var c = -rt_max * r_aa;
+        var numer = -b + Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+        r_t[i] = numer / (2 * a);
     }
-    return lambda;
+    return r_t
+}
+
+function compute_growth_rate(Naa, R, fa, volume) {
+    var lam = [];
+    var v_rt = compute_elong_rate_volume(r_aa, R, kd, fa, volume);
+    for (var i = 0; i < R.length; i++ ) { 
+        lam.push(3600 * v_rt[i] * R[i] * fa / Naa[i])
+    }
+    return lam
+}
+
+function compute_NAA(R) { 
+    var NAA = []
+    for (var i = 0 ; i < R.length; i++) { 
+    NAA.push(Math.exp(16.1) * Math.pow(R[i], 0.5));
+    }
+    return NAA
+}
+
+function compute_vol(R) { 
+    var vol = []
+    for (var i = 0; i < R.length; i++) {
+    vol.push(Math.exp(-5.8) * Math.pow(R[i], 0.6));
+    }
+    return vol
 }
--- code/figures/js/model_explorer.py
+++ code/figures/js/model_explorer.py
@@ -11,10 +11,11 @@ bokeh.io.output_file('model_explorer.html')
 
 
 # Define the data sources
-ribosomes = np.logspace(3, 6, 200)
+ribosomes = np.logspace(2, np.log10(3.3E5), 200)
 elongation_rate = [0]
 data_source = ColumnDataSource({'R': ribosomes, 
                                 'R_fa': ribosomes,
+                                'R_cell': ribosomes,
                                 'elongation_rate':np.zeros_like(ribosomes), 
                                 'elongation_rate_fa':np.zeros_like(ribosomes), 
                                 'lambda': np.zeros_like(ribosomes),
@@ -29,33 +30,34 @@ data_source = ColumnDataSource({'R': ribosomes,
 
 
 # Define the sliders. 
-kd_slider = Slider(value=5, start=0.01, end=100, step=0.01, title='Kd [mM]')
-r_aa = Slider(value=5, start=3, end=8, step=0.1, title='log\u2081\u2080 (amino acid supply rate [AA / (s•µm\u00b3)])')
-fa_slider = Slider(value=1, start=0.001, end=1, step=0.001, title='active ribosome fraction')
+kd_slider = Slider(value=5, start=0.01, end=50, step=0.01, title='dissociation constant [mM]',
+                  width=300)
+r_aa = Slider(value=8, start=1, end=10, step=0.1, title='log\u2081\u2080 (amino acid supply rate [AA / s•µm\u00b3])',
+              width=300)
+fa_slider = Slider(value=0.75, start=0.001, end=1, step=0.001, title='active ribosome fraction',
+            width=300)
 
 
 # Set up the figure canvases. 
-elong_rate_plot = bokeh.plotting.figure(width=500, height=500, 
+elong_rate_plot = bokeh.plotting.figure(width=500, height=350, 
                                       x_axis_label='ribosomes per µm\u00b3',
                                       x_axis_type='log',
                                       y_axis_label='elongation rate [AA / sec]',
-                                      x_range=[1E3, 5E5],
-                                      y_range = [0, 18])
-
-growth_rate_plot = bokeh.plotting.figure(width=500, height=500, 
-                                      x_axis_label='ribosomes per µm\u00b3',
+                                      x_range=[1E2, 5E5],
+                                      y_range = [0, 17.5])
+growth_rate_plot = bokeh.plotting.figure(width=500, height=350, 
+                                      x_axis_label='ribosomes per cell',
                                       x_axis_type='log',
                                       y_axis_label='growth rate [hr^-1]',
-                                      x_range=[1E3, 5E5],
-                                      y_range = [0, 2])
+                                      y_range=[0, 2])
 
-conc_plot = bokeh.plotting.figure(width=500, height=500, 
-                                      x_axis_label='ribosomes per µm\u00b3',
-                                      x_axis_type='log',
-                                      y_axis_type='log',
-                                      y_axis_label='AA / (s • µm\u00b3)',
-                                      x_range=[1E3, 5E5],
-                                      y_range=[1E0, 1E9])
+# conc_plot = bokeh.plotting.figure(width=500, height=350, 
+#                                       x_axis_label='ribosomes per µm\u00b3',
+#                                       x_axis_type='log',
+#                                       y_axis_type='log',
+#                                       y_axis_label='AA / (s • µm\u00b3)',
+#                                       x_range=[1E3, 5E5],
+#                                       y_range=[1E0, 1E9])
 
 
 
@@ -69,24 +71,26 @@ elong_rate_plot.line(x='R', y='elongation_rate', line_color=colors['blue'],
                      source=data_source, line_width=3, legend_label='no ribosome regulation')
 
 # Growth Rate Plot
-growth_rate_plot.line(x='R', y='lambda_fa',  line_width=3,
+growth_rate_plot.line(x='R_cell', y='lambda_fa',  line_width=3,
                      source=data_source, line_color=colors['light_blue'],
                       legend_field='leg')
-
-growth_rate_plot.line(x='R', y='lambda',  line_width=3, legend_label='no ribosome regulation',
+growth_rate_plot.line(x='R_cell', y='lambda',  line_width=3, legend_label='no ribosome regulation',
                      source=data_source, line_color=colors['blue'])
 
-# Concentration Plot
-conc_plot.line(x='R', y='supply', line_width=3, legend_label='[AA] generated',
-               line_color='black', source=data_source)
-conc_plot.line(x='R', y='consumed_fa', line_width=3, legend_label='[AA] consumed (with regulation)',
-              line_color=colors['light_red'], source=data_source)
-conc_plot.line(x='R', y='consumed', line_width=3, legend_label='[AA] consumed (no regulation)',
-              line_color=colors['red'], source=data_source)
-conc_plot.line(x='R', y='accum_fa', line_width=3, legend_label='[AA]_eff (with regulation)',
-              line_color=colors['light_purple'], source=data_source)
-conc_plot.line(x='R', y='accum', line_width=3, legend_label='[AA]_eff (no regulation)',
-              line_color=colors['purple'], source=data_source)
+elong_rate_plot.legend.location = 'top_left'
+growth_rate_plot.legend.location = 'top_left'
+
+# # Concentration Plot
+# conc_plot.line(x='R', y='supply', line_width=3, legend_label='[AA] generated',
+#                line_color='black', source=data_source)
+# conc_plot.line(x='R', y='consumed_fa', line_width=3, legend_label='[AA] consumed (with regulation)',
+#               line_color=colors['light_red'], source=data_source)
+# conc_plot.line(x='R', y='consumed', line_width=3, legend_label='[AA] consumed (no regulation)',
+#               line_color=colors['red'], source=data_source)
+# conc_plot.line(x='R', y='accum_fa', line_width=3, legend_label='[AA]_eff (with regulation)',
+#               line_color=colors['light_purple'], source=data_source)
+# conc_plot.line(x='R', y='accum', line_width=3, legend_label='[AA]_eff (no regulation)',
+#               line_color=colors['purple'], source=data_source)
 
 
 # Load the javascript. 
@@ -105,12 +109,8 @@ fa_slider.callback = cb
 
 # Define the layout
 controls = bokeh.layouts.row(kd_slider, r_aa, fa_slider)
-plots = bokeh.layouts.row(elong_rate_plot, growth_rate_plot, conc_plot)
-lay = bokeh.layouts.column(controls, plots)
+bottom_row = bokeh.layouts.row(elong_rate_plot, growth_rate_plot)
+lay = bokeh.layouts.column(controls, bottom_row)
 bokeh.io.save(lay)
-
-
-# %%
-
 
 # %%

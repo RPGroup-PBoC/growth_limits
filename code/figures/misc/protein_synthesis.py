e915e80d5562bbee829523ce387fa15dd1cedc2a
Nathan Belliveau <nbellive@caltech.edu>
2020-10-07 12:43:10 -0700

    reorganize

---
+++ code/figures/misc/protein_synthesis.py
@@ -0,0 +1,61 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import prot.viz
+colors, palette = prot.viz.bokeh_theme()
+prot.viz.plotting_style()
+
+# load the data 
+data = pd.read_csv('../../data/compiled_estimate_categories.csv')
+
+dataset_colors = {'li_2014':colors['purple'], 'schmidt_2016':colors['light_blue'],
+                   'peebo_2015':colors['green'], 'valgepea_2013':colors['red']}
+
+
+
+# tRNA SYNTHETASES
+fig, ax = plt.subplots(1, 1, figsize=(3, 2))
+ax.xaxis.set_tick_params(labelsize=6)
+ax.yaxis.set_tick_params(labelsize=6)
+ax.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+ax.set_ylabel('total number of\ntRNA synthetases per cell', fontsize=6)
+ax.set_yscale('log')
+ax.set_ylim([5E3, 5E5])
+ax.set_xlim([0, 2])
+_trna = data[data['shorthand']=='trna']
+ax.plot(0.5, 1E4,'o', ms=6, color=colors['dark_brown'], alpha=0.4, label='estimated value')
+for g, d in _trna.groupby(['dataset', 'dataset_name']):
+    ax.plot(d['growth_rate_hr'], d['n_complex'], 'o', ms=4, alpha=0.75,
+            color=dataset_colors[g[0]], markeredgecolor='k', markeredgewidth=0.5,
+            label=g[1])
+
+# Plot the dashed lines.
+ax.vlines(0.5, 1, 1E4, 'k', linestyle='--', label='__nolegend__', lw=0.75)
+ax.hlines(1E4, 0, 0.5, 'k', linestyle='--', label='__nolegend__', lw=0.75)
+ax.legend(fontsize=6)
+plt.savefig('../../figures/tRNA_synthase_plot.svg', bbox_inches='tight')
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(3, 2))
+ax.xaxis.set_tick_params(labelsize=6)
+ax.yaxis.set_tick_params(labelsize=6)
+ax.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+ax.set_ylabel('ribosomes per cell', fontsize=6)
+ax.set_yscale('log')
+ax.set_ylim([1E3, 5E5])
+ax.set_xlim([0, 2])
+_ribo = data[data['shorthand']=='ribosome']
+ax.plot(0.5, 1.5E4, 'o', ms=6, color=colors['dark_brown'], alpha=0.4, label='estimated value')
+for g, d in _ribo.groupby(['dataset', 'dataset_name']):
+    ax.plot(d['growth_rate_hr'], d['n_complex'], 'o', ms=4, alpha=0.75,
+            color=dataset_colors[g[0]], markeredgecolor='k', markeredgewidth=0.5,
+            label=g[1])
+
+# Plot the dashed lines.
+ax.vlines(0.5, 1, 1.5E4, 'k', linestyle='--', label='__nolegend__', lw=0.75)
+ax.hlines(1.5E4, 0, 0.5, 'k', linestyle='--', label='__nolegend__', lw=0.75)
+ax.legend(fontsize=6)
+plt.savefig('../../figures/ribosome_plot.svg', bbox_inches='tight')
+
+# %%
---
+++ code/figures/misc/ribosome_dependence.py
@@ -0,0 +1,63 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import prot.viz
+import prot.size
+colors, palette = prot.viz.bokeh_theme()
+prot.viz.plotting_style()
+
+# Load and restrict the data
+data = pd.read_csv('../../data/compiled_estimate_categories.csv')
+ribo = data[data['shorthand']=='ribosome']
+
+# Define the color palettes. 
+dataset_colors = {'li_2014':colors['purple'], 'schmidt_2016':colors['light_blue'],
+                   'peebo_2015':colors['green'], 'valgepea_2013':colors['red']}
+
+
+# Define some parameters
+growth_rate = np.linspace(0, 2, 200)
+t_double = growth_rate**-1 * 60 * 60
+DENSITY = 1.1 # in pg per fL
+PROT_FRAC = 0.15 # Fraction of dry mass that is protein
+AVG_PROT_MASS = 30E3 / (6E23 * 1E-12) # average protein mass in pg
+AVG_PROT_LENGTH = 300 # in Amino Acids
+AVG_AA_MASS = 110 / (6E23 * 1E-12)
+TRANSLATION_RATE = 17.1 # in AA per sec
+volume = prot.size.lambda2size(growth_rate)
+a = -0.89
+c = 4.60
+d = 0.922
+active_frac = a*np.exp(-c*growth_rate)+d
+
+
+# Compute the predicted scaling. 
+prediction = (DENSITY * volume * PROT_FRAC) /\
+             (AVG_AA_MASS * active_frac * TRANSLATION_RATE * t_double)
+
+
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(6,4))
+# Format the axes
+ax.set_yscale('log')
+ax.set_xlim([0, 2])
+ax.xaxis.set_tick_params(labelsize=6)
+ax.yaxis.set_tick_params(labelsize=6)
+ax.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+ax.set_ylabel('ribosomes per cell', fontsize=6)
+
+# Plot the prediction
+ax.plot(growth_rate, prediction, 'k--', lw=0.75, label='predicted trend')
+
+
+# Plot the measurements
+for g, d in ribo.groupby(['dataset', 'dataset_name']):
+    ax.plot(d['growth_rate_hr'], d['n_complex'], 'o', ms=4, 
+            color=dataset_colors[g[0]], markeredgecolor='k', markeredgewidth=0.5,
+            alpha=0.75, label=g[1])
+
+ax.legend(fontsize=6)
+plt.savefig('../../figures/ribosome_trend.pdf')
+# %%

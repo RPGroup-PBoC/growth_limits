9951fae300bf5b294e9c7f4e7abf1f34f053cf9f
Nathan Belliveau <nbellive@caltech.edu>
2020-10-09 12:06:23 -0700

    fig5-s1

--- code/figures/fig5-S1_COG_cytosol_barplot.py
+++ code/figures/fig5-S1_COG_cytosol_barplot.py
@@ -1,8 +1,10 @@
 #%%
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import prot.viz
 import prot.size as size
 colors, palette = prot.viz.bokeh_theme()
@@ -14,26 +16,124 @@ prot.viz.plotting_style()
 ######################
 # plot configuration #
 ######################
-fig = plt.figure(figsize = (4,4.5))#constrained_layout=True)
-# widths = [6, 7]
+fig = plt.figure()#figsize = (6,5))#constrained_layout=True)
+widths = [5, 6]
 # heights = [2, 0.5, 0.5, 0.25]
-widths = [3]
-heights = [1, 6]
-spec = fig.add_gridspec(ncols=1, nrows=2, width_ratios=widths,
-                          height_ratios=heights)
+# widths = [5, 5]
+heights = [1.75, 1.25, 1.5, 1.5]
+spec = fig.add_gridspec(ncols=2, nrows=4, width_ratios=widths)
+                          # height_ratios=heights)
 
 
-ax3 = fig.add_subplot(spec[0])
-ax4 = fig.add_subplot(spec[1])
+ax1 = fig.add_subplot(spec[:2,0])
+ax2 = fig.add_subplot(spec[2:,0])
 
-######################
-# Plot 2
-######################
+ax3 = fig.add_subplot(spec[0,1])
+ax4 = fig.add_subplot(spec[1:,1])
+
+
 # Load the data set
 data = pd.read_csv('../../data/compiled_absolute_measurements.csv')
 
 # total fg per cell cytosol GO:0005829 - cytosol
-data_cytosol = data[data.go_terms.str.contains('GO:0005737')]#0005829')]
+data_cytosol = data[data.go_terms.str.contains('GO:0005829')]
+
+data_cytosol = data_cytosol.replace('Not Assigned', 'poorly characterized or not assigned')
+data_cytosol = data_cytosol.replace('poorly characterized', 'poorly characterized or not assigned')
+#
+
+cog_class_order = ['metabolism',
+                'cellular processes and signaling',
+                'information storage and processing',
+                'poorly characterized or not assigned']
+order_dict = dict(zip(cog_class_order,
+                np.arange(4)))
+
+color_dict = dict(zip(cog_class_order,
+                    ['#679B48', '#BF703A', '#D3B15E', '#788FBD']))
+                    # ['', '#BF703A', '#D3B15E', '#788FBD']))
+
+marker_dict = dict(zip(data.dataset.unique(),
+                    ['o', 's', 'd', 'v']))
+
+label_dict = dict(zip(data.dataset_name.unique(),
+                    ['o', 's', 'd', 'v']))
+######################
+# Plot 1
+######################
+# relative dist of different COG categories
+
+
+# Compute the mass fraction
+mass_frac = []
+for g, d in data_cytosol.groupby(['dataset', 'condition', 'growth_rate_hr']):
+    tot_mass = d['fg_per_cell'].sum()
+    sector_mass = d.groupby(['dataset', 'dataset_name', 'condition', 'growth_rate_hr', 'cog_class'])['fg_per_cell'].sum().reset_index()
+    frac = sector_mass['fg_per_cell'].values / tot_mass
+    sector_mass['frac'] = frac
+    sector_mass['dataset_name'] = sector_mass['dataset_name']
+    mass_frac.append(sector_mass)
+mass_frac = pd.concat(mass_frac, sort=False)
+
+for g, d in mass_frac.groupby(['dataset', 'cog_class', 'dataset_name']):
+    ax1.plot(d.growth_rate_hr, d['frac'], marker = marker_dict[g[0]], color=color_dict[g[1]],
+                    alpha=0.75, markeredgecolor='k', markeredgewidth=0.25,
+                    label = g[2], ms=4, zorder=10, linewidth = 0)
+
+ax1.set_ylim(0,1)
+ax1.set_xlim(0,2)
+ax1.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+ax1.set_ylabel('relative cytosolic protein\nfraction (GO term : 0005829)', fontsize=6)
+ax1.xaxis.set_tick_params(labelsize=5)
+ax1.yaxis.set_tick_params(labelsize=5)
+
+legend_elements = [Line2D([0], [0],
+                    marker=label_dict[i],
+                    color='w', linewidth = 0,
+                    label=i, markeredgecolor='k',
+                    markeredgewidth=0.25,
+                    markerfacecolor='gray',
+                    markersize=4) for i in data.dataset_name.unique()]
+
+
+
+ax1.legend(handles=legend_elements, loc='upper left', fontsize = 6)
+
+
+######################
+# Plot 2
+######################
+# metabolism vs information processing
+
+cm = plt.cm.get_cmap('viridis')
+
+for g, d in mass_frac.groupby(['dataset', 'growth_rate_hr', 'dataset_name', 'condition']):
+    ax2.scatter(d[d.cog_class == 'information storage and processing']['frac'],
+                    d[d.cog_class == 'metabolism']['frac'], marker = marker_dict[g[0]],
+                    c = d.growth_rate_hr.unique(), cmap=cm,#'viridis',
+                    vmin=0, vmax=2, alpha=0.75, edgecolors='k', linewidths=0.25)
+
+ax2.set_ylim(0.25,0.65)
+ax2.set_xlim(0.25,0.55)
+ax2.set_xlabel('relative mass fraction,\ninformation storage and processing', fontsize=6)
+ax2.set_ylabel('relative mass fraction,\nmetabolism', fontsize=6)
+ax2.xaxis.set_tick_params(labelsize=5)
+ax2.yaxis.set_tick_params(labelsize=5)
+
+cbaxes = inset_axes(ax2, width="30%", height="5%", loc=3)
+cb = plt.colorbar(plt.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=0, vmax=2), cmap=cm),
+        cax=cbaxes, ticks=[0.,2], orientation='horizontal')
+# cbaxes.ax.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)#, rotation=270)
+cbaxes.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+cbaxes.xaxis.set_tick_params(labelsize=5)
+cbaxes.yaxis.set_tick_params(labelsize=5)
+cbaxes.xaxis.tick_top()
+cbaxes.xaxis.set_label_position('top')
+cb.outline.set_linewidth(0)
+
+######################
+# Plot 3 - protein concentration in cell
+######################
 
 data_cytosol_fg_summary = pd.DataFrame()
 for c, d in data_cytosol.groupby(['dataset', 'condition', 'growth_rate_hr']):
@@ -99,16 +199,7 @@ df_cyt_schmidt_['rel_fg_per_cell'] = df_cyt_schmidt_.groupby('condition').transf
 df_cyt_schmidt_ = df_cyt_schmidt_.sort_values(by=['growth_rate_hr', 'gene_name'], ascending = False)
 
 y_order = dict(zip(df_cyt_schmidt_.condition.unique(), np.arange(len(df_cyt_schmidt_.condition.unique()))))
-cog_class_order = ['metabolism',
-                'cellular processes and signaling',
-                'information storage and processing',
-                'poorly characterized or not assigned']
-order_dict = dict(zip(cog_class_order,
-                np.arange(4)))
-# color_dict = dict(zip(cog_class_order,
-#                     ['', '#C8715B', '#A587AA', '#788FBD']))
-color_dict = dict(zip(cog_class_order,
-                    ['', '#BF703A', '#D3B15E', '#788FBD']))
+
 
 
 for c, d in df_cyt_schmidt_.groupby('condition', sort=False):
@@ -144,8 +235,7 @@ ax4.set_xlim(0,1)
 ax4.set_ylim(0,len(df_cyt_schmidt_.condition.unique()))
 ax4.set_yticks(np.arange(len(df_cyt_schmidt_.condition.unique()))-0.5)
 ax4.set_yticklabels(df_cyt_schmidt_.condition.unique())
-ax4.set_xlabel('relative protein abundance', fontsize=6)
-ax4.set_xlabel('relative cytoplasmic protein\nabundance (GO term : 0005829)', fontsize=6)
+ax4.set_xlabel('relative cytosolic protein\nabundance (GO term : 0005829)', fontsize=6)
 ax4.xaxis.set_tick_params(labelsize=5)
 ax4.yaxis.set_tick_params(labelsize=5)
 
@@ -159,7 +249,6 @@ ax4_twin.xaxis.set_tick_params(labelsize=5)
 ax4_twin.yaxis.set_tick_params(labelsize=5)
 #
 plt.tight_layout()
-fig.savefig('../../figures/fig5-S1_COG_cytosol_barplot_2.svg')
-
+fig.savefig('../../figures/fig5-S1_COG_cytosol_barplot.svg')
 
 # %%
